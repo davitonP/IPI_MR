50f4b163e3c370756ace302929cc5d5af86b57f5
davitonP <velascod@uabc.edu.mx>
2018-03-25 22:12:21 -0700

    new commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-offset-5 col-md-4">
+            <div class="form-login">
+              <h4>Bienvenido</h4>
+              <input type="text" id="userName" class="form-control input-sm chat-input" placeholder="Nombre de usuario" />
+              </br>
+              <input type="password" id="userPassword" class="form-control input-sm chat-input" placeholder="contraseña" />
+              </br>
+              <div class="wrapper">
+              <span class="group-btn">
+                  <a href="welcome.php" class="btn btn-primary btn-md">Entrar <i class="fa fa-sign-in"></i></a>
+              </span>
+              </div>
+            </div>
+
+            <div class="form-Register">
+            <h4>Registrate</h4>
+            <input type="text" id="userNameR" class="form-control input-sm chat-input" placeholder="Nombre de usuario" />
+            </br>
+            <input type="text" id="usermailR" class="form-control input-sm chat-input" placeholder="Correo electronico" />
+            </br>
+            <input type="password" id="userPasswordR" class="form-control input-sm chat-input" placeholder="contraseña" />
+            </br>
+            <div class="wrapper">
+            <span class="group-btn">
+                <a href="/movieR/welcome" class="btn btn-primary btn-md">Registro <i class="fa fa-sign-in"></i></a>
+            </span>
+            </div>
+            </div>
+
+        </div>
+
+    </div>
+</div>
---
+++ movie.php
@@ -0,0 +1,29 @@
+<?php
+$directorio = 'static/movies/Terror/';
+
+echo "
+  <div class='w3-third w3-container w3-margin-bottom'>
+    <img src=".$directorio."laformadelagua.jpg alt='Norway' style='width:100%' class='w3-hover-opacity'>
+    <div class='w3-container w3-white'>
+      <p><b>Lorem Ipsum jaja</b></p>
+      <p>Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+      <button type='button' name='button'>Rentar</button>
+    </div>
+  </div>
+  <div class='w3-third w3-container w3-margin-bottom'>
+    <img src='' alt='Norway' style='width:100%' class='w3-hover-opacity'>
+    <div class='w3-container w3-white'>
+      <p><b>Lorem Ipsum jaja</b></p>
+      <p>Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+      <button type='button' name='button'>Rentar</button>
+    </div>
+  </div>
+  <div class='w3-third w3-container w3-margin-bottom'>
+    <img src='' alt='Norway' style='width:100%' class='w3-hover-opacity'>
+    <div class='w3-container w3-white'>
+      <p><b>Lorem Ipsum jaja</b></p>
+      <p>Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+      <button type='button' name='button'>Rentar</button>
+    </div>
+  </div>";
+?>
